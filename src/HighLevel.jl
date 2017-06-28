618bc035a80218eb5489959355f5dcecd4709377
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-06-28 12:30:51 -0400

    Define and test iteration over schedules for convenience

--- src/HighLevel.jl
+++ src/HighLevel.jl
@@ -1,4 +1,4 @@
-export Schedule, Employee, ScheduleWeights
+export Schedule, ScheduleWeights, Employee, EmployeeList
 
 struct ScheduleWeights
     day1::Vector{Int}
@@ -14,11 +14,37 @@ struct Schedule{T<:Number}
     day4::Vector{Tuple{T, T}}
     day5::Vector{Tuple{T, T}}
 end
-immutable Employee
-    name::String
-    avail::Schedule
-    specialty::Int
+## Define some convenience functions for ScheduleWeights
+_to_iter(weights::ScheduleWeights) = (getfield(weights, n) for n in fieldnames(weights))
+_lengths(weights::ScheduleWeights) = map(length, _to_iter(weights))
+Base.length(weights::ScheduleWeights) = sum(_lengths(weights))
+## Define iteration over a Schedule for convenience.
+_to_iter(sched::Schedule) = (getfield(sched, n) for n in fieldnames(sched))
+_lengths(sched::Schedule) = map(length, _to_iter(sched))
+Base.length(sched::Schedule) = sum(_lengths(sched))
+Base.start(::Schedule) = 1
+function Base.next{T}(sched::Schedule{T}, state::Int)
+    accum = 0
+    for n in fieldnames(sched)
+        vec = getfield(sched, n)
+        len = length(vec)
+        if state <= accum + len
+            return vec[state - accum], state + 1
+        end
+        accum += len
+    end
+    return (zero(T), zero(T)), state + 1
 end
+Base.done(sched::Schedule, state::Int) = state > length(sched)
+Base.eltype{T}(::Type{Schedule{T}}) = Tuple{T, T}
+function Base.getindex(sched::Schedule, i::Int)
+    done(sched, i) && throw(BoundsError(sched, i))
+    first(next(sched, i))
+end
+Base.endof(sched::Schedule) = length(sched)
+
+isvalid(sched::Schedule, weights::ScheduleWeights) = length(weights) == length(sched)
+
 function t_min{T}(vec::Vector{Tuple{T, T}}, overall::Tuple{T, T})
     isvalid(t::Tuple{T, T}) = first(t)<last(t)
     omin, omax = overall
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,20 +5,24 @@ using Base.Test
 const osched = [
     (9, 19),
     (8, 19),
-    (9, 19),
+    (9, 16),
     (8, 19),
     (9, 16)
 ]
 const emp1sched = Schedule(
+[(8, 10), (12, 15), (18,20)],
+[(8, 10), (12, 15)],
+[(9, -2), (10, 12)],
 [(9, 17)],
-[(9, 17)],
-[(9, 17)],
-[(9, 17)],
-[(9, 17)]
+[(9, 9), (13, 16)]
 )
 const emp1 = Employee("Max", emp1sched, 0)
-
-println(SPS.to_vec(emp1sched, 1//4))
+@test eltype(emp1sched) == typeof((8, 10))
+@test length(emp1sched) == 10
+@test first(emp1sched) == (8, 10) # Iteration is defined?
+@test emp1sched[3] == (18, 20) # Supports indexing
+@test emp1sched[end] == (13, 16) # Supports [end] syntax
+@test Schedule(emp1) == emp1sched
 
 println(SPS.s_min(emp1sched, osched))
 
