be0cfeef7cc9b9465c94c10806a7225add4eb262
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-11-10 10:42:45 -0500

    Turn on precompilation in SPS module

--- src/SPS.jl
+++ src/SPS.jl
@@ -1,8 +1,15 @@
+VERSION >= v"0.4.0-dev+6521" && __precompile__()
 module SPS
 
-include("HighLevel.jl")
+include("UIObjects.jl")
+
+include("UIObjectsSupport.jl")
 
 include("LowLevel.jl")
+
+function generateFunctional(el::EmployeeList)
+
+end
 
 include("precompile.jl")
 
